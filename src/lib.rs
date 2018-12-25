05cf1407dc3bd06ac674aeb8a28b11810e2b0f06
dekokun <shintaro.kurachi@gmail.com>
2018-12-25 21:57:11 +0900

    in rust 2018, unneed extern crate

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,6 @@ cfg_if! {
     // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
     // allocator.
     if #[cfg(feature = "wee_alloc")] {
-        extern crate wee_alloc;
         #[global_allocator]
         static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
     }
